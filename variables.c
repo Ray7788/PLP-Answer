dedbdd4b34171c707112d21425f384ca2abcb9d6
Ray <ray778@foxmail.com>
2022-11-06 12:24:05 +0000

    Create variables.c

---
+++ variables.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+
+    int int_var = 42;
+    double double_var = 24.000000;
+
+    printf("int_var: %d \n" 
+            "double_var: %f \n", 
+            int_var, double_var);
+
+    return 0;
+}
