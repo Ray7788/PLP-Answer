cf11b9d6d2fdf8fb8edd11e429a777c175e1f86e
Ray <ray778@foxmail.com>
2022-11-06 11:57:35 +0000

    Create compilation-errors.c

---
+++ compilation-errors.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    printf("This should work!\n");
+    return 0;
+}
+
+// check50 -l --ansi-log olivierpierre/comp26020-problems/2022-2023/week2-c-basics/02-compilation-errors
