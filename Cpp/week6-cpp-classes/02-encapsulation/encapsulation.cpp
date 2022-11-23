76e42283521a675ed6eae2b4f4cff09b8e66365d
Ray7788 <87214670+Ray7788@users.noreply.github.com>
2022-11-23 16:15:24 +0000

    正确版本

--- Cpp/week6-cpp-classes/02-encapsulation/encapsulation.cpp
+++ Cpp/week6-cpp-classes/02-encapsulation/encapsulation.cpp
@@ -1,37 +1,61 @@
 #include <iostream>
 
 class Rectangle {
-public:
+private:
     float length;
     float width;
-};
+public:
+    Rectangle(float len, float wid){
+        length = len;
+        width = wid;  
+        // std::cout << "Constructor call\n";
+    }
+    float rectangle_perimeter(void);
+
+    float get_length(void){
+        return length;
+    }
+
+    float get_width(void){
+        return width;
+    }
+};   
 
 class Circle {
-public:
+private:
     float radius;
+public:
+    Circle(float rad){
+        radius = rad;
+    }
+    float circle_circumference(void);
+    
+    float get_radius(void){
+        return radius;
+    }
 };
 
-float rectangle_perimeter(Rectangle *r) {
-    return 2.0 * (r->length + r->width);
+float Rectangle::rectangle_perimeter() {
+    return 2.0 * (length + width);
 }
 
-float circle_circumference(Circle *c) {
-    return 2.0 * 3.14 * c->radius;
+float Circle::circle_circumference() {
+    return 2.0 * 3.14 * radius;
 }
 
 int main(int argc, char **argv) {
-    Rectangle r;
-    Circle c;
+    Rectangle r(10,20);
+    Circle c(1);
 
-    r.length = 10;
-    r.width = 20;
+    // r.length = 10;
+    // r.width = 20;
 
-    c.radius = 1;
+    // c.radius = 1;
 
-	std::cout << "Rectangle l: " << r.length << ", w: " << r.width;
-	std::cout << ", perimeter: " << rectangle_perimeter(&r) << "\n";
+	std::cout << "Rectangle l: " << r.get_length() << ", w: " << r.get_width();
+	std::cout << ", perimeter: " << r.rectangle_perimeter() << "\n";
 
-	std::cout << "Circle r: " << c.radius;
-	std::cout << ", circumference: " << circle_circumference(&c) << "\n";
+	std::cout << "Circle r: " << c.get_radius();
+	std::cout << ", circumference: " << c.circle_circumference() << "\n";
     return 0;
 }
