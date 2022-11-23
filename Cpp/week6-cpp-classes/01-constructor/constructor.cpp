de5843ee1510d7ae3b8b843fbb3770a06e23cd8c
Ray7788 <87214670+Ray7788@users.noreply.github.com>
2022-11-23 09:33:18 +0000

    错误版本1

--- Cpp/week6-cpp-classes/01-constructor/constructor.cpp
+++ Cpp/week6-cpp-classes/01-constructor/constructor.cpp
@@ -1,16 +1,21 @@
 #include <iostream>
 
 class Pair {
-public:
-    int x;
-    int y;
+    private:
+        int x;
+        int y;
+
+    public: 
+        Pair(int x, int y):
+            x{x},y{y} {}; 
+         
 };
 
 int main(int argc, char **argv) {
-    Pair p;
+    Pair p(10,20);
 
-    p.x = 10;
-    p.y = 20;
+    // p.x = 10;
+    // p.y = 20;
 
 	std::cout << "p.x is " << p.x << ", p.y is " << p.y << "\n";
 
