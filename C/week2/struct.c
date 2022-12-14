61c833c3b23c31e442fd3d0ccfcf88d86b194a1b
Ray7788 <ray778@foxmail.com>
2022-12-14 14:52:41 +0000

    正确版本

--- C/week2/struct.c
+++ C/week2/struct.c
@@ -1,42 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// unfinished!!!!
+// It's finished and works well now!!!!
 struct timestamp {
     unsigned int hour;
     unsigned int minute;
     unsigned int second;
 };
 
-void add_timestamps(struct timestamp *a,  struct timestamp *b,  struct timestamp *c){
-    c->hour = a->hour + b->hour;
-    c->minute = a->minute + b->minute;
-    c->second = c->second + c->second;
+struct timestamp add_timestamps(struct timestamp a, struct timestamp b){
+    // declare a new timestamp that holds the result
+    struct timestamp c;
+    
+    c.hour = a.hour + b.hour;
+    c.minute = a.minute + b.minute;
+    c.second = a.second + b.second;
 
-    if((c->second) >= 60){
-        c->second = c->second %60;
-        c->minute++;
+    if((c.second) > 59){
+        c.minute++;
+        c.second -= 60;
     } 
 
-    if((c->minute) >= 60){
-        c->minute = c->minute %60;
-        c->hour++;
+    if((c.minute) > 59){
+        c.hour++;
+        c.minute -= 60;
     }
+
+    return c;
 }
 
-int main(int argc, char *argv[]) {
-    struct timestamp a,b,c;
+int main(int argc, char **argv) {
+    // if command line argument count is not 6, print error and exit program
+    if(argc != 7){
+        printf("Please provide 6 commandline arguments");
+        return -1;
+    }
+
+    struct timestamp a,b;
 
     a.hour = atoi(argv[1]);
     a.minute = atoi(argv[2]);
-    a.second = (argv[3]);
+    a.second = atoi(argv[3]);
 
     b.hour = atoi(argv[4]);
     b.minute = atoi(argv[5]);    
-    c.second = atoi(argv[6]);
+    b.second = atoi(argv[6]);
 
-    add_timestamps(&a, &b, &c);
-    printf("%d %d %d",c.hour,c.minute,c.second);
+    struct timestamp c = add_timestamps(a, b);
+
+    printf("%d %d %d\n",c.hour,c.minute,c.second);
 
     return 0;
 }
