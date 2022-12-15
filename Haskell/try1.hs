b68cd3f5dfc70f32a82eb25fe645e94ad5905852
Rui Xu <rui.xu-11@student.manchester.ac.uk>
2022-12-15 12:16:18 +0000

    第四章 Haskell函数的语法 before is completed

--- Haskell/try1.hs
+++ Haskell/try1.hs
@@ -4,9 +4,6 @@
 This is a multi-line comment
 -}
 
--- main  = do
---     max 4 5
-
 doubleMe x y = x + y
 
 doubleSmallNumber x = if x > 100                           
@@ -19,7 +16,7 @@ addThree :: Int -> Int -> Int -> Int
 addThree x y z = x + y + z
 
 
-
+-- 在定义函数时，你可以为不同的模式分别定义函数体，这就让代码更加简洁易读。你可以匹配一切数据类型---数字，字符，List，元组，等等
 lucky :: (Integral a) => a -> String   
 lucky 7 = "LUCKY NUMBER SEVEN!"   
 lucky x = "Sorry, you're out of luck, pal!"   
@@ -49,3 +46,53 @@ tell [] = "The list is empty"
 tell (x:[]) = "The list has one element: " ++ show x   
 tell (x:y:[]) = "The list has two elements: " ++ show x ++ " and " ++ show y   
 tell (x:y:_) = "This list is long. The first two elements are: " ++ show x ++ " and " ++ show y  
+
+--  对list做模式匹配的方法
+head' :: [a] -> a   
+head' [] = error "Can't call head on an empty list, dummy!"   
+head' (x:_) = x  
+
+--  Guards
+{-模式用来检查一个值是否合适并从中取值，而 guard 则用来检查一个值的某项属性是否为真。
+咋一听有点像是 ​if ​语句，实际上也正是如此。不过处理多个条件分支时 guard 的可读性要高些，并且与模式匹配契合的很好。
+-}
+bmiTell :: (RealFloat a) => a -> String  
+bmiTell bmi  
+    | bmi <= 18.5 = "You're underweight, you emo, you!"  
+    | bmi <= 25.0 = "You're supposedly normal. "  
+    | bmi <= 30.0 = "You're fat! Lose some weight, fatty!"  
+    -- 最后的那个 guard 往往都是 ​otherwise​，它的定义就是简单一个 ​otherwise = True​ ，捕获一切。这与模式很相像，只是模式检查的是匹配，而它们检查的是布尔表达式 。
+    | otherwise   = "You're a whale, congratulations!"
+
+-- 我们的 where 关键字跟在 guard 后面(最好是与竖线缩进一致)，可以定义多个名字和函数。这些名字对每个 guard 都是可见的，这一来就避免了重复。
+bmiTell2 :: (RealFloat a) => a -> a -> String  
+bmiTell2 weight height  
+    | bmi <= 18.5 = "You're underweight, you emo, you!"  
+    | bmi <= 25.0 = "You're supposedly normal. Pffft, I bet you're ugly!"  
+    | bmi <= 30.0 = "You're fat! Lose some weight, fatty!"  
+    | otherwise   = "You're a whale, congratulations!"  
+    where bmi = weight / height ^ 2
+
+-- let 绑定与 where 绑定很相似。where 绑定是在函数底部定义名字，对包括所有 guard 在内的整个函数可见。
+-- let 绑定则是个表达式，允许你在任何位置定义局部变量，而对不同的 guard 不可见。
+cylinder :: (RealFloat a) => a -> a -> a  
+cylinder r h = 
+    let sideArea = 2 * pi * r * h  
+        topArea = pi * r ^2  
+    in  sideArea + 2 * topArea
+
+-- let 的格式为 let [bindings] in [expressions]。
+-- 在 let 中绑定的名字仅对 in 部分可见。let 里面定义的名字也得对齐到一列。不难看出，这用 where 绑定也可以做到。
+
+-- Haskell 取了这一概念融合其中。如其名，case 表达式就是，嗯，一种表达式。
+-- 跟 if..else 和 let 一样的表达式。用它可以对变量的不同情况分别求值，还可以使用模式匹配。
+describeList :: [a] -> String  
+describeList xs = "The list is " ++ case xs of [] -> "empty."  
+                                               [x] -> "a singleton list."   
+                                               xs -> "a longer list."
+
+describeList2 :: [a] -> String  
+describeList2 xs = "The list is " ++ what xs  
+    where what [] = "empty."  
+          what [x] = "a singleton list."  
+          what xs = "a longer list."
