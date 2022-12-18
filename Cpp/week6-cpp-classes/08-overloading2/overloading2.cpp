3c0c4d36091df3c4973a51a62d062bab7c465610
Ray7788 <ray778@foxmail.com>
2022-12-18 13:41:37 +0000

    正确版本

--- Cpp/week6-cpp-classes/08-overloading2/overloading2.cpp
+++ Cpp/week6-cpp-classes/08-overloading2/overloading2.cpp
@@ -10,6 +10,8 @@ public:
     Complex(int real_part, int imaginary_part);
     int get_real_part();
     int get_imaginary_part();
+
+    Complex operator+(Complex& other);      // 重构overload
 };
 
 Complex::Complex(int real_part, int imaginary_part) 
@@ -24,12 +26,27 @@ int Complex::get_imaginary_part() {
 }
 
 /* implement operator overrides here */
+// 求和运算符重载
+Complex Complex::operator+(Complex& other) {
+    return Complex(real + other.real, imag + other.imag);
+}
+
+// 输出运算符重载
+std::ostream& operator<<(std::ostream& out, Complex c) {
+    return std::cout << "(" << c.get_real_part() << "+" << c.get_imaginary_part() << "i)";
+}
+
+void add_and_print(Complex c1, Complex c2) {
+    std::cout << c1 << " + " << c2 << " = " << c1 + c2 << "\n";
+}
 
 int main(int argc, char **argv) {
     Complex c1(4, 5);
     Complex c2(9, 11);
     
-    std::cout << c1 + c2 << "\n"; // should print (13+16i)
+    add_and_print(c1, c2); // should print "(4+5i) + (9+11i) = (13+16i)"
 
     return 0;
 }
+
+//  本练习答案可在week7第一个ftemplates找到
