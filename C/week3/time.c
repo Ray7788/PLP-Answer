e3071df15b092b371c45d1f3e28737a41b403757
Ray7788 <87214670+Ray7788@users.noreply.github.com>
2023-05-08 19:27:54 +0100

    Bug fixes

--- C/week3/time.c
+++ C/week3/time.c
@@ -1,13 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
 
 int main(int argc, char *argv[]) {
-    float n;
-    n = atoi(argv[1]);
+    if (argc != 2) {
+        printf("Usage: %s <n>\n", argv[0]);
+        return 1;
+    }
     
-    // Add time duration
+    int n = atoi(argv[1]);
+    time_t start = time(NULL);
     sleep(n);
-    printf("sleep duration: %f seconds\n", n);
+    time_t end = time(NULL);
+    double duration = difftime(end, start);
+    printf("sleep duration: %.6f seconds\n", duration);
 
     return 0;
 }
+
+// check50 -l --ansi-log olivierpierre/comp26020-problems/2022-2023/week3-c-pointers-stdlib/07-time
