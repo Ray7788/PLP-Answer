53d85ac3f0189f70f49060428eaad28645d715b2
Ray7788 <87214670+Ray7788@users.noreply.github.com>
2023-05-16 17:39:52 +0100

    review

--- Cpp/week6-cpp-classes/01-constructor/constructor.cpp
+++ Cpp/week6-cpp-classes/01-constructor/constructor.cpp
@@ -5,14 +5,15 @@ class Pair {
 public: 
     int x;
     int y;
-    // 这是构造函数
-    Pair(int x, int y){
-        this->x = x;
-        this->y = y;
+    // 这是构造函数 C++新版写法
+    Pair(int x, int y): x(x), y(y){
+        // this->x = x;
+        // this->y = y;
     }
 };
 
 int main(int argc, char **argv) {
+    // 封装数据
     Pair p(10, 20);
 	std::cout << "p.x is " << p.x << ", p.y is " << p.y << "\n";
 
