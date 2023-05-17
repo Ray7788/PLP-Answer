224ee4e775ae5b1f51033d0ac01ba68a0019b370
Ray7788 <87214670+Ray7788@users.noreply.github.com>
2023-05-17 16:59:52 +0100

    1

---
+++ Rust/Basic/wk9_8b.rs
@@ -0,0 +1,17 @@
+fn main() {
+    let mut i = 10;
+    let j = i;
+
+    //need to declare i with 'let mut'
+    i = 11;
+
+    //note the value of i is not changed, even though the function argment is mutable
+    increment(i);
+
+    println!("{}",i);// 11
+    println!("{}",j);// 10 unchanged
+}
+
+fn increment(mut x: u32) {
+    x = x + 1;
+}
