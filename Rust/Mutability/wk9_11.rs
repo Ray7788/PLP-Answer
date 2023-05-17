7a6d1757bd5bb03d513465ee14747f761955f6e7
Ray7788 <87214670+Ray7788@users.noreply.github.com>
2023-05-17 17:55:54 +0100

    1

---
+++ Rust/Mutability/wk9_11.rs
@@ -0,0 +1,14 @@
+fn main() {
+    let mut s = String::from("hello");
+
+    s.push_str(" world!");
+
+    let t = &mut s;
+
+    t.push_str(" And universe!");
+
+    //causes error: try to borrow immutable s
+    println!("{}",s);
+    println!("{}",t);
+}
+// mutable borrow can't coexist with other borrow (t) 
