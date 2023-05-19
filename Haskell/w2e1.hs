17d3ec547b6979679985238b335820b9cb894673
Ray7788 <87214670+Ray7788@users.noreply.github.com>
2023-05-19 21:41:49 +0100

    1

---
+++ Haskell/w2e1.hs
@@ -0,0 +1,50 @@
+-- TYPES AND TYPE INFERENCE
+
+-- we can explicitly give the type of a function
+f :: Int -> Int
+f x = x + 1
+
+-- Note: Int is fixed precision, Integer unbounded
+-- We also have Float, Double
+
+fd :: Double -> Double
+fd x = x + 1
+
+-- ok:
+-- main = print(fd 10)
+-- not ok:
+-- main = print(fd (10::Int))
+-- main = print((1::Int) + (1::Integer))
+
+big :: Int -> Bool
+big x = x > 10
+
+-- main = print(big 11)
+
+-- In Haskell, every expression has a type
+-- How come last week we didn't have to
+-- delcare them? Haskell infers types
+-- we don't give explicitly E.g.
+j :: Bool -> Bool
+j x = True
+
+-- main = print (j False)  -- 会输出True
+
+-- What is the type of j?
+-- We can give it several types, like
+
+-- jj :: Int -> Bool
+jj x = False
+
+-- Note we can't give it type Bool -> Int!
+-- But the original has a strange feature:
+
+main = print (jj 1 && jj True)
+
+-- Is this 'dynamic typing'? No it is called
+-- 'parametic polymorphism'. We can use
+-- type variables to indicate that something
+-- works for all types!
+-- 泛型
+jjj :: a -> Bool
+jjj x = False
