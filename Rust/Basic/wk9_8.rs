83440f25a9587c051abcdc442dc1163a3b5aaf09
Ray7788 <87214670+Ray7788@users.noreply.github.com>
2023-05-20 22:57:14 +0100

    纠错i和1

--- Rust/Basic/wk9_8.rs
+++ Rust/Basic/wk9_8.rs
@@ -3,7 +3,7 @@ fn main() {
     let j = i;
 
     //need to declare i with 'let mut'
-    // or we can use let 1 = 11;  "shadowing" 
+    // or we can use let i = 11;  "shadowing" 
     i = 11;
 
     println!("{}",i);
