6b944de7ba13800d62ceacc9d9fd630e9509e7f9
Ray7788 <87214670+Ray7788@users.noreply.github.com>
2023-05-21 21:19:56 +0100

    2

--- Haskell/w2e2.hs
+++ Haskell/w2e2.hs
@@ -9,31 +9,26 @@ sumPair (x, y) = x + y
 
 -- main = print(sumPair(2,3))
 
--- Similarly, given two types we can
--- construct the type of functions from one to
--- the other
+-- Similarly, given two types we can construct the type of functions from one to the other
 
 -- Bool是最终输出
 atTen :: (Int -> Bool) -> Bool
 atTen f = f 10
 -- 与10无关，重点是input是x和True
 
-main = print (atTen (\x -> True))
+-- main = print (atTen (\x -> False))
 
 -- note this is an example of type inference!
 -- we could write explicitly
 
 -- main = print(atTen ((\x -> True):: Int -> Bool))
 
--- We can use more than one type variable
--- to indicate which inputs have to have
--- matching types (Generic type)
+-- We can use more than one type variable to indicate which inputs have to have matching types (Generic type)
 myApp :: (a -> b) -> a -> b
 myApp f x = f x
 
 -- main = print(myApp (\x -> True) 10)
--- note that it would also be correct
--- to give myApp a less general type e.g.
+-- note that it would also be correct to give myApp a less general type e.g.
 
 -- myApp :: (Int -> Bool) -> Int -> Bool
 
@@ -41,7 +36,6 @@ myApp f x = f x
 -- main = print(myApp (\x -> True) 10)
 
 -- but also
-
 -- myApp :: (a -> a) -> a -> a
 
 -- which doesn't! But it works for e.g.
