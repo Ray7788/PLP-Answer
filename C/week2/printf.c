9a97f58d8eaf97621cb6a2a02c9a24c0a482bf59
Rui(Ray) Xu <ray778@foxmail.com>
2023-06-13 12:52:40 +0100

    1

---
+++ C/week2/printf.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+// This is a program displaying a large 11x9 characters 'C' using dashes
+int main(){
+    // \n refers to a 'new line' character.
+    printf("    ######\n");
+    printf("  ##      ##\n");
+    printf(" #\n");
+    printf(" #\n");
+    printf(" #\n");
+    printf(" #\n");
+    printf(" #\n");
+    printf("  ##      ##\n");
+    printf("    ######\n");
+    return 0;
+}
+
+
+//     ######
+//   ##      ##
+//  #
+//  #
+//  #
+//  #
+//  #
+//   ##      ##
+//     ######
+
+
+// check50 -l --ansi-log olivierpierre/comp26020-problems/2022-2023/week2-c-basics/01-printf
